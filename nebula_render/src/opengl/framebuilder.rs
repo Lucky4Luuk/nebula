3c3dd5fd40500e27b4c0b8bff1928e9d2c8c0ffc
Luuk van Oijen <lazyluuk.channel@gmail.com>
2021-10-16 01:13:26 +0200

    basic docs

--- nebula_render/src/opengl/framebuilder.rs
+++ nebula_render/src/opengl/framebuilder.rs
@@ -5,22 +5,24 @@ pub struct FrameBuilder<'gl> {
 }
 
 impl<'gl> FrameBuilder<'gl> {
-    pub fn new(gl: &'gl Gl) -> Self {
+    pub(crate) fn new(gl: &'gl Gl) -> Self {
         gl.make_current();
         Self {
             gl: gl,
         }
     }
 
+    /// Finish rendering this frame
+    pub fn finish(self) {
+        self.gl.make_not_current();
+        self.gl.swap_buffers();
+    }
+
+    /// Clears the screen with a certain colour
     pub fn clear(&self, rgba: (f32, f32, f32, f32)) {
         unsafe {
             self.gl.ClearColor(rgba.0, rgba.1, rgba.2, rgba.3);
             self.gl.Clear(gl_bindings::COLOR_BUFFER_BIT);
         }
-    }
-
-    pub fn finish(self) {
-        self.gl.make_not_current();
-        self.gl.swap_buffers();
     }
 }
